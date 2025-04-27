0d522ba729fa88d9f3aade35ec6492dfa5f94cab
htakuya <taku08121113@gmail.com>
2025-04-27 23:16:43 +0900

    Devise関連のメッセージを日本語化

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module TechlogApp
       g.helper false          # helperファイルを自動生成しない
       g.test_framework :rspec # テストフレームワークをrspecに設定
     end
+
+    config.i18n.default_locale = :ja
   end
 end
